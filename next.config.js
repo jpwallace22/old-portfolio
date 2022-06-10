113319ebddd524df62e898aa64d1699a716c0955
jpwallace22 <93415734+jpwallace22@users.noreply.github.com>
2022-06-10 08:45:41 -0700

    Add boilerplate to _document for FOUC

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@ module.exports = {
   images: {
     domains: ['localhost', 'justinwallace.dev'],
   },
-  compiler: {
-    styledComponents: true,
-  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
