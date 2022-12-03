de1ceca67d99dbd226c0c3700ea561d87bb95543
jpwallace22 <jpwallace22@gmail.com>
2022-12-03 12:15:40 -0800

    Revert "Purge extra .ts and set styled-components to pure"
    This reverts commit 15cb6a0efcd16b65dd8029354c5c6c081d6a03e0.

--- src/theme/getAppTheme.ts
+++ src/theme/getAppTheme.ts
@@ -1,6 +1,13 @@
 import { darkButton, lightButton } from 'molecules/Button/button.theme';
+import { darkButtonGroup, lightButtonGroup } from 'molecules/ButtonGroup/buttonGroup.theme';
+import { darkCheckbox, lightCheckbox } from 'molecules/Checkbox/checkbox.theme';
 import { darkCircularProgress, lightCircularProgress } from 'molecules/CircularProgress/circularProgress.theme';
 import { darkLinearProgress, lightLinearProgress } from 'molecules/LinearProgress/linearProgress.theme';
+import { darkRadio, lightRadio } from 'molecules/Radio/radio.theme';
+import { darkSelect, lightSelect } from 'molecules/Select/Select.theme';
+import { darkSlider, lightSlider } from 'molecules/Slider/slider.theme';
+import { darkSwitch, lightSwitch } from 'molecules/Switch/switch.theme';
+import { darkTextField, lightTextField } from 'molecules/TextField/TextField.theme';
 import { darkTooltip, lightTooltip } from 'molecules/Tooltip/tooltip.theme';
 
 import { darkPalette, lightPalette, standardPalette } from 'theme/colorPalettes';
@@ -13,8 +20,15 @@ export const getAppTheme = (mode: 'light' | 'dark') => {
   return {
     components: {
       ...(isDark ? darkButton : lightButton),
+      ...(isDark ? darkTextField : lightTextField),
+      ...(isDark ? darkSwitch : lightSwitch),
+      ...(isDark ? darkRadio : lightRadio),
       ...(isDark ? darkTooltip : lightTooltip),
+      ...(isDark ? darkButtonGroup : lightButtonGroup),
       ...(isDark ? darkLinearProgress : lightLinearProgress),
+      ...(isDark ? darkSlider : lightSlider),
+      ...(isDark ? darkCheckbox : lightCheckbox),
+      ...(isDark ? darkSelect : lightSelect),
       ...(isDark ? darkFormControl : lightFormControl),
       ...(isDark ? darkCircularProgress : lightCircularProgress),
     },
