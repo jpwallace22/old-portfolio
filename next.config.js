3ecfd31c03e7e9ded2ffe20cf1088e5ab522884f
jpwallace22 <jpwallace22@gmail.com>
2022-12-03 16:09:12 -0800

    Add next/analyze

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+// const withBundleAnalyzer = require('@next/bundle-analyzer')({
+//   enabled: process.env.ANALYZE === 'true',
+// });
+
+// module.exports = withBundleAnalyzer({});
 module.exports = {
   images: {
     domains: ['localhost', 'justinwallace.dev', 'www.datocms-assets.com'],
