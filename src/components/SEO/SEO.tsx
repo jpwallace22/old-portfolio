e7799201a7c478ca97f1f0c5cb43b032a4888ec1
jpwallace22 <jpwallace22@gmail.com>
2022-12-11 11:54:34 -0800

    Add optional children for extra meta tags

--- src/components/SEO/SEO.tsx
+++ src/components/SEO/SEO.tsx
@@ -1,28 +1,35 @@
-import React from 'react';
-
 import type { FileField } from 'graphql/generatedTypes';
 import type { FC } from 'react';
 
 interface ISeo {
-  title: string;
-  description: string;
-  slug: string;
-  image: FileField;
+  title?: string;
+  description?: string;
+  slug?: string;
+  image?: FileField;
+  type?: string;
+  children?: JSX.IntrinsicElements['meta'];
 }
 
-const SEO: FC<ISeo> = ({ description, title, image, slug }) => {
+const SEO: FC<ISeo> = ({ description, title, image, slug, type, children }) => {
   const metaDesc =
     description ||
     'This is the personal site and portfolio of Justin Wallace. Here I showcase some works and the progress of my career. I also write about dev-related experiences.';
   const metaTitle = title || 'Justin Wallace Development';
   const metaImage = image?.url || 'https://www.datocms-assets.com/85391/1670693320-jwdev.webp';
+  const metaImageAlt = image?.alt || 'The dark mode style tile of Justin Wallace Development';
+  const metaType = type || 'website';
 
   return (
     <>
+      <title>{title}</title>
       <meta name="description" content={metaDesc} />
+      <meta property="og:type" content={metaType} />
       <meta property="og:image" content={metaImage} />
+      <meta property="og:image:type" content="image/webp" />
+      <meta property="og:image:alt" content={metaImageAlt} />
       <meta property="og:title" content={metaTitle} />
       <meta name="og:url" content={`https://www.justinwallace.dev/${slug}`} />
+      {children}
     </>
   );
 };
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,6 +3,8 @@ import { Fragment } from 'react';
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
+import SEO from 'components/SEO/SEO';
+
 import type { DocumentContext } from 'next/document';
 
 class MyDocument extends Document {
@@ -35,19 +37,15 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Darker+Grotesque:wght@700;900&family=IBM+Plex+Sans:wght@200;300;400;600;700&display=fallback"
-            rel="stylesheet"
-          /> */}
           <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
           <link rel="manifest" href="/favicons/site.webmanifest" />
+          <meta property="og:site_name" content="Justin Wallace Development" />
           <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#5bbad5" />
           <meta name="msapplication-TileColor" content="#00aba9" />
           <meta name="theme-color" content="#ffffff" />
+          <SEO />
         </Head>
         <body>
           <Main />
